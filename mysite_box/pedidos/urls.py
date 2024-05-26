9cd222185f517d4572851f7a6bdb97623ebd2d4b
LVO <112906515+d75324@users.noreply.github.com>
2024-05-26 15:46:59 -0300

    login, logout working

--- mysite_box/pedidos/urls.py
+++ mysite_box/pedidos/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('pedidos/', views.pedidos, name='pedidos'),
+    #path('login/', views.login_users, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('customers/', views.customers, name='customers'),
     path('pedidos/', views.customer_list_view, name='pedidos'),
 ]
--- mysite_box/pedidos/views.py
+++ mysite_box/pedidos/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render, redirect
 from .models import Customer
 from .forms import form_customers
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html')
+    # chequear que estoy logueado
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # autenticar
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Acceso Correcto")
+            return redirect ('home')
+        else:
+            messages.success(request, "Revisar la información proporcionada")
+            return redirect ('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Hasta pronto!")
+    return redirect ('home')
+
 
 def pedidos(request):
     return render(request, 'pedidos.html')
